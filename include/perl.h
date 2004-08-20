ea55573d08dbd1203b519174a99e23bf8c1e3b2c
zowie <zowie>
2004-08-20 03:00:39 +0000

    included perl.h in the include dir.

---
+++ include/perl.h
@@ -0,0 +1,16 @@
+/* Convenient perl interface macros for the perl side of FLUX.
+ *
+ * You need to include the perl api and FLUX api stuff yourself.
+ */
+
+
+#define SvWorld(wsv,name) (   ( SvROK(wsv) && sv_derived_from((wsv),"Flux::World") )  ? (WORLD *)(SvIVx(SvRV(wsv))) : ( croak("%s requires a Flux::World.\n",(name)), (WORLD *)0 ) )
+
+#define SvFluxon(wsv,name) (   ( SvROK(wsv) && sv_derived_from((wsv),"Flux::Fluxon") )  ? (FLUXON *)(SvIVx(SvRV(wsv))) : ( croak("%s requires a Flux::Fluxon.\n",(name)), (FLUXON *)0 ) )
+
+
+#define SvConc(wsv,name) (   ( SvROK(wsv) && sv_derived_from((wsv),"Flux::Concentration") )  ? (FLUX_CONCENTRATION *)(SvIVx(SvRV(wsv))) : ( croak("%s requires a Flux::Concentration.\n",(name)), (FLUX_CONCENTRATION *)0 ) )
+
+#define SvVertex(wsv,name) (   ( SvROK(wsv) && sv_derived_from((wsv),"Flux::Vertex") )  ? (VERTEX *)(SvIVx(SvRV(wsv))) : ( croak("%s requires a Flux::Vertex.\n",(name)), (FLUX_CONCENTRATION *)0 ) )
+
+
