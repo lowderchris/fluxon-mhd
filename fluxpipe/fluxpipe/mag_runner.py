dcd5c0949cd0e399e442f550e226c3b7eba39f35
gillyspace27 <gilly@swri.org>
2023-10-09 16:16:44 -0600

    linted everywhere but pipe_helper.py

--- fluxpipe/fluxpipe/mag_runner.py
+++ fluxpipe/fluxpipe/mag_runner.py
@@ -37,9 +37,6 @@ if capture:
         print("\n    >>verbose = True. All stdout from the processes will be printed following each iteration.<<\n")
 else:
     pass
-    # print("\n\nProcessing the following CR: ", rotations, "\n\n")
-    # print(f"\n\n Batch Name {batch_name} \n\n")
-    
 
 to_break = 0
 print("")
@@ -55,7 +52,7 @@ with tqdm(total=len(rotations), unit="rotation") as pbar:
                 # do_flux = [8000]
 
                 for nflux in do_flux:
-                    result = subprocess.run(["perl", pdl_script_path, str(rot), str(reduction), str(do_download), 
+                    result = subprocess.run(["perl", pdl_script_path, str(rot), str(reduction), str(do_download),
                                              str(recompute), str(nflux), str(batch_name), str(plot_only), str(ADAPT)], capture_output=capture)
                     # exit()
                     if capture and verbose:
@@ -65,10 +62,10 @@ with tqdm(total=len(rotations), unit="rotation") as pbar:
                         if to_break > 2:
                             break
             else:
-                result = subprocess.run(["perl", pdl_script_path, str(rot), str(reduction), str(do_download), 
+                result = subprocess.run(["perl", pdl_script_path, str(rot), str(reduction), str(do_download),
                                          str(recompute), str(nflux), str(batch_name), str(plot_only), str(ADAPT)], capture_output=capture)
                 if capture and verbose:
-                    
+
                     print(result.stdout.decode())
                 if result.returncode != 0:
                     to_break += 1
@@ -80,7 +77,7 @@ with tqdm(total=len(rotations), unit="rotation") as pbar:
 
         except Exception as e:
             print(e)
-        
+
 
 
 
